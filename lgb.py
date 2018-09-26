53227233ad9e67bfbfe8c9ec4e080b82c32607d1
lsvih <lsvih@qq.com>
2018-09-26 21:42:48 +0800

    Add lgb model.

--- lgb.py
+++ lgb.py
@@ -1,6 +1,6 @@
 import warnings
 
-import xgboost as xgb
+import lightgbm as lgb
 from sklearn.model_selection import StratifiedKFold
 
 from utils import *
@@ -8,18 +8,28 @@ from utils import *
 warnings.filterwarnings('ignore')
 
 fold = 5
-param1 = {'max_depth': 6, 'silent': 1, 'objective': 'multi:softprob', 'num_class': 3}
-param4 = {'learning_rate': 0.05, 'n_estimators': 400, 'max_depth': 6, 'min_child_weight': 1, 'seed': 0,
-          'subsample': 0.8, 'colsample_bytree': 0.8, 'gamma': 0, 'reg_alpha': 0, 'reg_lambda': 1, 'n_jobs': -1,
-          'num_class': 8, 'silent': 1, 'objective': 'multi:softprob'}
-model_path = './temp/xgb/'
+param1 = {'max_depth': 6, 'objective': 'multiclass', 'num_class': 3, "lambda_l1": 0.1, "lambda_l2": 0.2, 'n_jobs': -1}
+param4 = {'learning_rate': 0.05, 'max_depth': 6, 'min_child_weight': 1, 'seed': 0,
+          'subsample': 0.8, 'colsample_bytree': 0.8, 'reg_alpha': 0, 'reg_lambda': 1, 'n_jobs': -1,
+          'num_class': 8, 'objective': 'multiclass', "lambda_l1": 0.1, "lambda_l2": 0.2}
+model_path = './temp/lgb/'
 if not os.path.exists(model_path):
     os.mkdir(model_path)
 
 
+def f1_score_vali(preds, data_vali):
+    labels = data_vali.get_label()
+    if 199929 >= preds.shape[0] > 199900:
+        preds = np.argmax(preds.reshape(3, -1), axis=0)
+    else:
+        preds = np.argmax(preds.reshape(8, -1), axis=0)
+    score_vali = f1_score(y_true=labels, y_pred=preds, average='weighted')
+    return 'f1_score', score_vali, True
+
+
 def train_model(X, y, test, params, model_num) -> list:
     print('This is model %d' % model_num)
-    X_test = xgb.DMatrix(data=test)
+    X_test = test
     xx_score = []
     cv_pred = []
     skf = StratifiedKFold(n_splits=fold, random_state=2018, shuffle=True)
@@ -27,16 +37,15 @@ def train_model(X, y, test, params, model_num) -> list:
         print(index, '/', fold)
         X_train, X_valid, y_train, y_valid = \
             X.iloc[train_index], X.iloc[test_index], y.iloc[train_index], y.iloc[test_index]
-        train_data = xgb.DMatrix(data=X_train, label=y_train)
-        validation_data = xgb.DMatrix(data=X_valid, label=y_valid)
-        clf = xgb.train(params, train_data, num_boost_round=2000,
-                        evals=[(train_data, 'train'), (validation_data, 'eval')], early_stopping_rounds=50,
-                        feval=f1_score_vali, verbose_eval=1)
+        train_data = lgb.Dataset(data=X_train, label=y_train)
+        validation_data = lgb.Dataset(data=X_valid, label=y_valid)
+        clf = lgb.train(params, train_data, num_boost_round=100000, valid_sets=[validation_data],
+                        early_stopping_rounds=50, feval=f1_score_vali, verbose_eval=1)
         clf.save_model(model_path + str(model_num) + '_' + str(index))
-        xx_pred = clf.predict(validation_data)
+        xx_pred = clf.predict(X_valid, num_iteration=clf.best_iteration)
         xx_pred = np.argmax(xx_pred, axis=1)
         xx_score.append(f1_score(y_valid, xx_pred, average='weighted'))
-        y_test = clf.predict(X_test)
+        y_test = clf.predict(X_test, num_iteration=clf.best_iteration)
         y_test = np.argmax(y_test, axis=1)
         if index == 0:
             cv_pred = np.array(y_test).reshape(-1, 1)
